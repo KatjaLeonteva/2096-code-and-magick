55b9c46dcef3af38a4f993826f622a01fc31c1d9
Katya Leontyeva <k.leontyeva.mail@gmail.com>
2018-01-28 11:41:16 +0300

    Added statistics bars with labels

--- js/stat.js
+++ js/stat.js
@@ -6,14 +6,18 @@ var CLOUD_RADIUS = 10;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var SHADOW_GAP = 10;
+var CHART_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var TEXT_HEIGHT = 30;
 
 /**
  * Отрисовывает прямоугольник с закругленными краями фиксированной ширины и высоты
  * заданного цвета в заданных координатах
  *
  * @param {object} ctx Контекст отрисовки.
- * @param {number} x Координата по оси X от левого верхнего края.
- * @param {number} y Координата по оси Y от левого верхнего края.
+ * @param {number} x Координата по оси X.
+ * @param {number} y Координата по оси Y.
  * @param {string} color Цвет облака.
  */
 var renderCloud = function (ctx, x, y, color) {
@@ -43,20 +47,18 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 /**
- * Отрисовывает текст сообщения
+ * Отрисовывает текст сообщения о победе
  *
  * @param {object} ctx Контекст отрисовки.
  * @param {string} message Текст сообщения.
- * @param {number} x Координата по оси X от левого верхнего края.
- * @param {number} y Координата по оси Y от левого верхнего края.
  */
 var renderMessage = function (ctx, message) {
   // Координаты сообщения
-  var messageX = CLOUD_X + CLOUD_WIDTH / 2;
+  var messageX = CLOUD_X + CLOUD_WIDTH / 2; // Середина облака по горизонтали
   var messageY = CLOUD_Y + 10; // 10 - отступ от верхнего края облака
 
   // Стиль текста сообщения
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.textAlign = 'center';
@@ -65,9 +67,54 @@ var renderMessage = function (ctx, message) {
   // Каждая строка отрисовывается отдельно.
   var lines = message.split('\n');
   for (var i = 0; i < lines.length; i++) {
-    ctx.fillText(lines[i], messageX, messageY + i * 16); // 16 - размер шрифта
+    ctx.fillText(lines[i], messageX, messageY + i * 16);
   }
 };
+
+/**
+ * Отрисовывает подпись к графику
+ *
+ * @param {object} ctx Контекст отрисовки.
+ * @param {string} label Текст подписи.
+ * @param {number} x Координата по оси X.
+ * @param {number} y Координата по оси Y
+ */
+var renderLabel = function (ctx, label, x, y) {
+  ctx.fillStyle = '#000000';
+  ctx.font = '14px PT Mono';
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.fillText(label, x, y);
+};
+
+/**
+ * Возвращает наибольшее значение в массиве
+ *
+ * @param {array} arr Массив значений.
+ * @return {number} maxElement Наибольшее значение.
+ */
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+/**
+ * Возвращает случайное число между min (включительно) и max (не включая)
+ *
+ * @param {number} min Минимальное значение.
+ * @param {number} max Максимальное значение.
+ * @return {number} Случайное число.
+ */
+function getRandomArbitary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 /**
  * Отрисовывает окно статистики при окончании игры
@@ -75,9 +122,8 @@ var renderMessage = function (ctx, message) {
  *
  * @param {object} ctx Контекст отрисовки.
  * @param {array} names Имена игороков.
- * @param {attay} times Соответствующее время прохождения игры.
+ * @param {array} times Соответствующее время прохождения игры.
  */
-
 window.renderStatistics = function (ctx, names, times) {
   // Отрисовывает тень облака с отступами по горизонтали и вертикали
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
@@ -86,4 +132,25 @@ window.renderStatistics = function (ctx, names, times) {
 
   // Отрисовывает сообщение по центру
   renderMessage(ctx, 'Ура вы победили!\nСписок результатов:');
+
+  // Отрисовывает статистику
+  var maxTime = getMaxElement(times);
+  var bottomY = CLOUD_Y + CLOUD_HEIGHT;
+
+  for (var i = 0; i < times.length; i++) {
+    var barHeight = (CHART_HEIGHT * times[i]) / maxTime;
+    var barX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var barY = bottomY - barHeight - TEXT_HEIGHT;
+    var barOpacity = getRandomArbitary(0.05, 1); // Прозрачность минимум 5%, иначе график плохо видно
+
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (names[i] !== 'Вы') {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + barOpacity + ')';
+    }
+    ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
+
+    var textX = barX + BAR_WIDTH / 2;
+    renderLabel(ctx, names[i], textX, bottomY - TEXT_HEIGHT / 2);
+    renderLabel(ctx, Math.floor(times[i], 0), textX, bottomY - barHeight - TEXT_HEIGHT / 2 - TEXT_HEIGHT);
+  }
 };
