80a00a2b703e289b04d650fd085e9a15dbefc122
Katya Leontyeva <k.leontyeva.mail@gmail.com>
2018-02-12 22:12:03 +0300

    Revert "Added drag and drop for dialog and artifacts"
    This reverts commit 55f792e6ccd465b3a203924a2d07a243697f794f.

--- js/setup.js
+++ js/setup.js
@@ -20,39 +20,4 @@
 
   // Изменение цвета фаербола персонажа
   window.utils.colorizeElement(fireball, 'fireball', fireballInput);
-
-  // Перетаскивание артефактов
-  var shopElement = document.querySelector('.setup-artifacts-shop');
-  var artifactsElement = document.querySelector('.setup-artifacts');
-  var draggedItem = null;
-
-  shopElement.addEventListener('dragstart', function (evt) {
-    if (evt.target.tagName.toLowerCase() === 'img') {
-      draggedItem = evt.target;
-      evt.dataTransfer.setData('text/plain', evt.target.alt);
-    }
-    artifactsElement.style.outline = '2px dashed red';
-  });
-
-  artifactsElement.addEventListener('dragover', function (evt) {
-    evt.preventDefault();
-    return false;
-  });
-
-  artifactsElement.addEventListener('drop', function (evt) {
-    evt.target.style.backgroundColor = '';
-    evt.target.appendChild(draggedItem);
-    evt.preventDefault();
-    artifactsElement.style.outline = 'none';
-  });
-
-  artifactsElement.addEventListener('dragenter', function (evt) {
-    evt.target.style.backgroundColor = 'yellow';
-    evt.preventDefault();
-  });
-
-  artifactsElement.addEventListener('dragleave', function (evt) {
-    evt.target.style.backgroundColor = '';
-    evt.preventDefault();
-  });
 })();
