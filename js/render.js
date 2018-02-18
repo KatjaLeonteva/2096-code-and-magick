1fbe30e8f6584bfa29efb7cb02c311cbb28879e1
Katya Leontyeva <k.leontyeva.mail@gmail.com>
2018-02-18 19:30:48 +0300

    Get wizards data from server

--- js/render.js
+++ js/render.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+  // Количество похожих волшебников
+  var WIZARDS_NUM = 4;
+
   // Элемент, в который мы будем вставлять похожих волшебников
   var WIZARDS_LIST_ELEMENT = document.querySelector('.setup-similar-list');
 
@@ -35,15 +38,44 @@
     var wizardElement = wizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizardData.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizardData.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizardData.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizardData.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizardData.colorEyes;
 
     return wizardElement;
   };
 
-  // Отрисовываем волшебников
-  renderWizards(window.wizards, WIZARDS_LIST_ELEMENT, WIZARD_TEMPLATE);
+  var successHandler = function (response) {
+    // Берем 4 произвольных записи из полученных данных
+    var wizards = [];
+    var allWizards = response.slice();
+    for (var i = 0; i < WIZARDS_NUM; i++) {
+      wizards.push(getRandomElementUnique(allWizards));
+    }
 
-  // Показываем блок с похожими персонажами
-  document.querySelector('.setup-similar').classList.remove('hidden');
+    // Отрисовываем волшебников
+    renderWizards(wizards, WIZARDS_LIST_ELEMENT, WIZARD_TEMPLATE);
+
+    // Показываем блок с похожими персонажами
+    document.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.classList.add('error-message');
+    node.textContent = errorMessage;
+    document.body.appendChild(node);
+  };
+
+  /**
+   * Возвращает случайный элемент и удаляет его из исходного массива
+   *
+   * @param {array} arr Массив для поиска элемента.
+   * @return {string} Случайный элемент массива.
+   */
+  function getRandomElementUnique(arr) {
+    var removedEl = arr.splice(Math.random() * arr.length, 1);
+    return removedEl[0];
+  }
+
+  window.backend.load(successHandler, errorHandler);
 })();
